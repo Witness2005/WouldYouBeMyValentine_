b045eb14d5b595fb0c9dae9d28df85618a583ebd
Witness2005 <jupablota2005@gmail.com>
2026-02-10 16:20:49 -0500

    Grid Añadido

--- main.py
+++ main.py
@@ -9,20 +9,31 @@ ctk.set_default_color_theme("blue")
 class App(ctk.CTk):
     def __init__(self):
         super().__init__()
+        #Columnas
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_columnconfigure(1, weight=0)
+        self.grid_columnconfigure(2, weight=1)
+        #Columnas
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_rowconfigure(1, weight=1)
 
 
         self.title("My love proposal")
         self.geometry("400x240")
 
 
-        self.label = ctk.CTkLabel(self, text="Texto de Prueba", font=("Arial", 20))
-        self.label.pack(pady=40)
+        self.label = ctk.CTkLabel(self, text="¿Quieres ser mi San Valentin?", font=("Arial", 20))
+        self.label.grid(row=0, column=1, padx=10, pady=10)
 
-        self.button = ctk.CTkButton(self, text="Click", command=self.button_callback)
-        self.button.pack(pady=10)
+        self.yes_button = ctk.CTkButton(self, text="Siiii", command=self.button_callback)
+        self.yes_button.grid(row=1, column=0, padx=10, pady=10)
+
+        self.no_button = ctk.CTkButton(self, text="noooo", command=self.button_callback)
+        self.no_button.grid(row=1, column=2, padx=10, pady=10)
 
     def button_callback(self):
         print("Botón presionado")
+
 
 
 if __name__ == "__main__":
